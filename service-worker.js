e022906719bb783c8b56616e1f924e2b756fa4f2
MRMods1 <moramadanmods@gmail.com>
2025-07-06 13:30:27 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,23 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'mrmods-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://yt3.googleusercontent.com/S9OBrJ-3VPK8kmNAOe7FktIe8SolU_zDRkDgBQTLuJNFeQUEzrfW3QmV9vlDzj_XU-Ildj4euMU=s160-c-k-c0x00ffffff-no-rj'
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('pwa-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/service-worker.js',
-        '/styles.css',
-        '/script.js'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
